620ebe2108f17e2ed4d048594f9899cec9922610
Michal <michal.kwiatek.92@gmail.com>
2023-03-17 23:48:09 +0100

    add sample screens and navigation between them

--- app/src/main/java/pl/klubmobile/klubmobileapp/MainActivity.kt
+++ app/src/main/java/pl/klubmobile/klubmobileapp/MainActivity.kt
@@ -4,10 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.rememberNavHostEngine
+import com.ramcosta.composedestinations.utils.isRouteOnBackStack
 import pl.klubmobile.klubmobileapp.bottomNavbar.BottomNavItem
 import pl.klubmobile.klubmobileapp.ui.theme.KlubMobileAppTheme
 
@@ -21,33 +35,74 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val engine = rememberNavHostEngine()
+                    val navController = engine.rememberNavController()
+                    val startRoute = NavGraphs.root.startRoute
+
+                    navController.currentBackStackEntryAsState().value?.print()
+
                     Scaffold(
-                        bottomBar = { BottomNavbar() }
-                    ) {}
+                        bottomBar = { BottomNavbar(navController) }
+                    ) { contentPadding ->
+                        DestinationsNavHost(
+                            engine = engine,
+                            navController = navController,
+                            navGraph = NavGraphs.root,
+                            modifier = Modifier.padding(contentPadding),
+                            startRoute = startRoute
+                        )
+                    }
                 }
             }
         }
     }
 }
+
 @Composable
-fun BottomNavbar() {
-    var selectedItem by remember { mutableStateOf(0) }
-    val items = listOf(BottomNavItem.AboutClub)
-    BottomNavigation{
-        items.forEachIndexed { index, item ->
+private fun BottomNavbar(navController: NavHostController) {
+    BottomNavigation {
+        BottomNavItem.values().forEach { destination ->
+            val isCurrentDestOnBackStack = navController.isRouteOnBackStack(destination.direction)
             BottomNavigationItem(
-                icon = { Icon(item.icon, contentDescription = item.label)},
-                label = {Text(text = item.label)},
-                selected = selectedItem == index,
-                onClick = { selectedItem = index }
+                icon = { Icon(destination.icon, contentDescription = destination.label) },
+                label = { Text(text = destination.label) },
+                selected = isCurrentDestOnBackStack,
+                onClick = {
+                    if (isCurrentDestOnBackStack) {
+                        // When we click again on a bottom bar item and it was already selected
+                        // we want to pop the back stack until the initial destination of this bottom bar item
+                        navController.popBackStack(destination.direction.route, false)
+                        return@BottomNavigationItem
+                    }
+                    navController.navigate(destination.direction.route) {
+                        // Pop up to the root of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        popUpTo(NavGraphs.root.route) {
+                            saveState = true
+                        }
+
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                }
             )
         }
     }
 }
+
+private fun NavBackStackEntry.print(prefix: String = "stack") {
+    val stack = destination.route
+    println("$prefix = $stack")
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     KlubMobileAppTheme {
-        BottomNavbar()
+        BottomNavbar(NavHostController(LocalContext.current))
     }
 }
--- app/src/main/java/pl/klubmobile/klubmobileapp/bottomNavbar/BottomNavItem.kt
+++ app/src/main/java/pl/klubmobile/klubmobileapp/bottomNavbar/BottomNavItem.kt
@@ -3,7 +3,29 @@ package pl.klubmobile.klubmobileapp.bottomNavbar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.ui.graphics.vector.ImageVector
+import pl.klubmobile.klubmobileapp.destinations.AboutScreenDestination
+import pl.klubmobile.klubmobileapp.destinations.BlogsScreenDestination
+import pl.klubmobile.klubmobileapp.destinations.CalendarScreenDestination
+import pl.klubmobile.klubmobileapp.destinations.DirectionDestination
 
-sealed class BottomNavItem(var label: String, var icon: ImageVector) {
-    object AboutClub : BottomNavItem(label = "About Club", icon = Icons.Filled.Info)
+enum class BottomNavItem(
+    val direction: DirectionDestination,
+    val label: String,
+    val icon: ImageVector
+) {
+    ABOUT(
+        direction = AboutScreenDestination,
+        label = "About Club",
+        icon = Icons.Filled.Info
+    ),
+    CALENDAR(
+        direction = CalendarScreenDestination,
+        label = "Calendar",
+        icon = Icons.Filled.Info
+    ),
+    BLOGS(
+        direction = BlogsScreenDestination,
+        label = "Blogs",
+        icon = Icons.Filled.Info
+    )
 }
