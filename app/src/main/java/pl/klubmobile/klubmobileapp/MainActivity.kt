77f796b6ac41b6c0242f71d9653a76fa39d703dc
Kasiaak <zuzanna.dawiec@gmail.com>
2023-03-18 13:20:16 +0100

    Fixed imports, extract "surface" to function, fix naming convention, code formatting

--- app/src/main/java/pl/klubmobile/klubmobileapp/MainActivity.kt
+++ app/src/main/java/pl/klubmobile/klubmobileapp/MainActivity.kt
@@ -4,7 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,37 +23,44 @@ class MainActivity : ComponentActivity() {
         setContent {
             KlubMobileAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Scaffold(
-                        bottomBar = { BottomNavbar() }
-                    ) {}
-                }
+                MainScreen()
             }
         }
     }
+
+    @Composable
+    private fun MainScreen() {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Scaffold(
+                bottomBar = { BottomNavBar() }
+            ) {}
+        }
+    }
 }
+
 @Composable
-fun BottomNavbar() {
+fun BottomNavBar() {
     var selectedItem by remember { mutableStateOf(0) }
     val items = listOf(BottomNavItem.AboutClub)
-    BottomNavigation{
+    BottomNavigation {
         items.forEachIndexed { index, item ->
             BottomNavigationItem(
-                icon = { Icon(item.icon, contentDescription = item.label)},
-                label = {Text(text = item.label)},
+                icon = { Icon(item.icon, contentDescription = item.label) },
+                label = { Text(text = item.label) },
                 selected = selectedItem == index,
                 onClick = { selectedItem = index }
             )
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     KlubMobileAppTheme {
-        BottomNavbar()
+        BottomNavBar()
     }
 }
